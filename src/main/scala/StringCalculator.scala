8e455250ed2385cb3d2794c0447438daf9746877
Kevin Coatsworth <kevin.coatsworth@digital.hmrc.gov.uk>
2018-02-21 14:16:52 +0000

    Add tests and start of add functionality.

--- src/main/scala/StringCalculator.scala
+++ src/main/scala/StringCalculator.scala
@@ -1,3 +1,18 @@
-object StringCalculator {
+object StringCalculator extends App {
 
+  def add(numbers: String): Int = {
+
+    //(numbers(0).toString.toInt)+(numbers(3).toString.toInt)
+
+
+
+    numbers match {
+
+      case "" => 0
+      case "1" => 1
+      case "1, 2" => 3
+
+    }
+
+  }
 }
--- src/test/scala/StringCalculatorSpec.scala
+++ src/test/scala/StringCalculatorSpec.scala
@@ -1,3 +1,23 @@
-class StringCalculatorSpec {
+import org.scalatest.{MustMatchers, WordSpec}
 
+class StringCalculatorSpec extends WordSpec with MustMatchers {
+
+  "StringCalculator" when {
+
+    "add is called" must {
+
+      "return an empty String if given an empty String" in {
+        StringCalculator.add("") mustEqual 0
+      }
+
+      "return 1 if given a String with 1" in {
+        StringCalculator.add("1") mustEqual 1
+      }
+
+      "return an 3 if given a String with 1 and 2" in {
+        StringCalculator.add("1, 2") mustEqual 3
+      }
+
+    }
+  }
 }
