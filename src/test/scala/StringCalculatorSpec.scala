22248bae9abd97e1018c6fd68af3dd8f13bdcb4c
Kevin Coatsworth <kevin.coatsworth@digital.hmrc.gov.uk>
2018-02-21 17:57:54 +0000

    refactor add method for positive numbers.

--- src/test/scala/StringCalculatorSpec.scala
+++ src/test/scala/StringCalculatorSpec.scala
@@ -14,16 +14,36 @@ class StringCalculatorSpec extends WordSpec with MustMatchers {
         StringCalculator.add("1") mustEqual 1
       }
 
-      "return 3 if given a String with 1 and 2" in {
-        StringCalculator.add("1, 2") mustEqual 3
+      "return 42 if given a String with 42" in {
+        StringCalculator.add("42") mustEqual 42
       }
 
-      "return 15 if given a String with 1, 2, 3, 4, 5" in {
-        StringCalculator.add("1, 2, 3, 4, 5") mustEqual 15
+      "return 3 if given a String with 1,2" in {
+        StringCalculator.add("1,2") mustEqual 3
       }
 
-      "return 15 if given a String with 1, 2, 3, 4, 5, 10, 69" in {
-        StringCalculator.add("1, 2, 3, 4, 5, 10, 69") mustEqual 94
+      "return 6 if given a String with 1,2,3" in {
+        StringCalculator.add("1,2,3") mustEqual 6
+      }
+
+      "return 15 if given a String with 1,2,3,4,5" in {
+        StringCalculator.add("1,2,3,4,5") mustEqual 15
+      }
+
+      "return 94 if given a String with 1,2,3,4,5,10,69" in {
+        StringCalculator.add("1,2,3,4,5,10,69") mustEqual 94
+      }
+
+      "return 6 if given a String with 1\n2,3" in {
+        StringCalculator.add("1\n2,3") mustEqual 6
+      }
+
+      "return 3 if given a String with //;\n1;2" in {
+        StringCalculator.add("//;\n1;2") mustEqual 3
+      }
+
+      "return an IllegalArgumentException if given a String with 1,-2,-3" ignore {
+        StringCalculator.add("1,-2,-3") mustEqual "requirement failed: "
       }
 
     }
